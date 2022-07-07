d5d7dd9075b6448803dad1362a15d8b1c70c2648
Wade Barnes <wade@neoterictech.ca>
2022-07-07 11:28:20 -0700

    Update configuration
    - Add support for copying genesis files from the local machine.
    - Update meta variables.
    - Add genesis information for provisional test network.
    - Update docs
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>
    Signed-off-by: Sylvain Martel <sylvain.martel10@mcn.gouv.qc.ca>

--- terraform/meta/test-aws-variables.tf
+++ terraform/meta/test-aws-variables.tf
@@ -1,42 +1,42 @@
 # ===================================================
 # General
 # ---------------------------------------------------
-resource "tfe_variable" "test_instance_count" {
+resource "tfe_variable" "test_aws_instance_count" {
   key          = "candy_instance_count"
   value        = 2
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_use_elastic_ips" {
+resource "tfe_variable" "test_aws_use_elastic_ips" {
   key          = "candy_use_elastic_ips"
   value        = true
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_region" {
+resource "tfe_variable" "test_aws_region" {
   key          = "candy_region"
   value        = "ca-central-1"
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_application_name" {
+resource "tfe_variable" "test_aws_application_name" {
   key          = "candy_application_name"
   value        = "candy"
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_environment" {
+resource "tfe_variable" "test_aws_environment" {
   key          = "candy_environment"
   value        = "test"
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_instance_name" {
+resource "tfe_variable" "test_aws_instance_name" {
   key          = "candy_instance_name"
   value        = "test-bc-aws"
   category     = "terraform"
@@ -47,7 +47,7 @@ resource "tfe_variable" "test_instance_name" {
 # ===================================================
 # AMI
 # ---------------------------------------------------
-resource "tfe_variable" "test_ami_owners" {
+resource "tfe_variable" "test_aws_ami_owners" {
   key          = "candy_ami_owners"
   value        = "[\"099720109477\"]"
   hcl          = true
@@ -55,7 +55,7 @@ resource "tfe_variable" "test_ami_owners" {
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_ami_filter_name" {
+resource "tfe_variable" "test_aws_ami_filter_name" {
   key          = "candy_ami_filter_name"
   value        = "[\"ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*\"]"
   hcl          = true
@@ -63,7 +63,7 @@ resource "tfe_variable" "test_ami_filter_name" {
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_ami_filter_root_device_type" {
+resource "tfe_variable" "test_aws_ami_filter_root_device_type" {
   key          = "candy_ami_filter_root_device_type"
   value        = "[\"ebs\"]"
   hcl          = true
@@ -71,7 +71,7 @@ resource "tfe_variable" "test_ami_filter_root_device_type" {
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_ami_filter_virtualization_type" {
+resource "tfe_variable" "test_aws_ami_filter_virtualization_type" {
   key          = "candy_ami_filter_virtualization_type"
   value        = "[\"hvm\"]"
   hcl          = true
@@ -84,7 +84,7 @@ resource "tfe_variable" "test_ami_filter_virtualization_type" {
 # EC2
 #   - t3.large (for dev and test): 2CPU, 8GiB Memory
 # ---------------------------------------------------
-resource "tfe_variable" "test_ec2_instance_type" {
+resource "tfe_variable" "test_aws_ec2_instance_type" {
   key          = "candy_ec2_instance_type"
   value        = "t3.large"
   category     = "terraform"
@@ -96,35 +96,35 @@ resource "tfe_variable" "test_ec2_instance_type" {
 # EBS
 #   - 20G should be fine for dev.
 # ---------------------------------------------------
-resource "tfe_variable" "test_ebs_volume_size" {
+resource "tfe_variable" "test_aws_ebs_volume_size" {
   key          = "candy_ebs_volume_size"
   value        = "20"
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_ebs_volume_type" {
+resource "tfe_variable" "test_aws_ebs_volume_type" {
   key          = "candy_ebs_volume_type"
   value        = "gp2"
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_ebs_encrypted" {
+resource "tfe_variable" "test_aws_ebs_encrypted" {
   key          = "candy_ebs_encrypted"
   value        = true
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_ebs_kms_key_id" {
+resource "tfe_variable" "test_aws_ebs_kms_key_id" {
   key          = "candy_ebs_kms_key_id"
   value        = ""
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_ebs_delete_on_termination" {
+resource "tfe_variable" "test_aws_ebs_delete_on_termination" {
   key          = "candy_ebs_delete_on_termination"
   value        = true
   category     = "terraform"
@@ -135,14 +135,14 @@ resource "tfe_variable" "test_ebs_delete_on_termination" {
 # ===================================================
 # Subnet
 # ---------------------------------------------------
-resource "tfe_variable" "test_subnet_cidr_block_prefix" {
+resource "tfe_variable" "test_aws_subnet_cidr_block_prefix" {
   key          = "candy_subnet_cidr_block_prefix"
   value        = "172.31."
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-aws.id
 }
 
-resource "tfe_variable" "test_subnet_cidr_starting_address" {
+resource "tfe_variable" "test_aws_subnet_cidr_starting_address" {
   key          = "candy_subnet_cidr_starting_address"
   value        = 100
   category     = "terraform"
@@ -153,7 +153,7 @@ resource "tfe_variable" "test_subnet_cidr_starting_address" {
 # ===================================================
 # SG
 # ---------------------------------------------------
-resource "tfe_variable" "test_sg_description" {
+resource "tfe_variable" "test_aws_sg_description" {
   key          = "candy_sg_description"
   value        = "Validator Node Security Group"
   category     = "terraform"
--- terraform/meta/test-azure-variables.tf
+++ terraform/meta/test-azure-variables.tf
@@ -3,42 +3,42 @@
 # ===================================================
 # General
 # ---------------------------------------------------
-resource "tfe_variable" "alpha_instance_count" {
+resource "tfe_variable" "test_azure_instance_count" {
   key          = "candy_instance_count"
   value        = 2
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-azure.id
 }
 
-# resource "tfe_variable" "alpha_use_elastic_ips" {
+# resource "tfe_variable" "test_azure_use_elastic_ips" {
 #   key          = "candy_use_elastic_ips"
 #   value        = true
 #   category     = "terraform"
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_region" {
+# resource "tfe_variable" "test_azure_region" {
 #   key          = "candy_region"
 #   value        = "ca-central-1"
 #   category     = "terraform"
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-resource "tfe_variable" "alpha_application_name" {
+resource "tfe_variable" "test_azure_application_name" {
   key          = "candy_application_name"
   value        = "candy"
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-azure.id
 }
 
-resource "tfe_variable" "alpha_environment" {
+resource "tfe_variable" "test_azure_environment" {
   key          = "candy_environment"
   value        = "test"
   category     = "terraform"
   workspace_id = data.tfe_workspace.test-azure.id
 }
 
-resource "tfe_variable" "alpha_instance_name" {
+resource "tfe_variable" "test_azure_instance_name" {
   key          = "candy_instance_name"
   value        = "test-bc-azure"
   category     = "terraform"
@@ -49,7 +49,7 @@ resource "tfe_variable" "alpha_instance_name" {
 # # ===================================================
 # # AMI
 # # ---------------------------------------------------
-# resource "tfe_variable" "alpha_ami_owners" {
+# resource "tfe_variable" "test_azure_ami_owners" {
 #   key          = "candy_ami_owners"
 #   value        = "[\"099720109477\"]"
 #   hcl          = true
@@ -57,7 +57,7 @@ resource "tfe_variable" "alpha_instance_name" {
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_ami_filter_name" {
+# resource "tfe_variable" "test_azure_ami_filter_name" {
 #   key          = "candy_ami_filter_name"
 #   value        = "[\"ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*\"]"
 #   hcl          = true
@@ -65,7 +65,7 @@ resource "tfe_variable" "alpha_instance_name" {
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_ami_filter_root_device_type" {
+# resource "tfe_variable" "test_azure_ami_filter_root_device_type" {
 #   key          = "candy_ami_filter_root_device_type"
 #   value        = "[\"ebs\"]"
 #   hcl          = true
@@ -73,7 +73,7 @@ resource "tfe_variable" "alpha_instance_name" {
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_ami_filter_virtualization_type" {
+# resource "tfe_variable" "test_azure_ami_filter_virtualization_type" {
 #   key          = "candy_ami_filter_virtualization_type"
 #   value        = "[\"hvm\"]"
 #   hcl          = true
@@ -86,7 +86,7 @@ resource "tfe_variable" "alpha_instance_name" {
 # # EC2
 # #   - t3.large (for dev and test): 2CPU, 8GiB Memory
 # # ---------------------------------------------------
-# resource "tfe_variable" "alpha_ec2_instance_type" {
+# resource "tfe_variable" "test_azure_ec2_instance_type" {
 #   key          = "candy_ec2_instance_type"
 #   value        = "t3.large"
 #   category     = "terraform"
@@ -98,35 +98,35 @@ resource "tfe_variable" "alpha_instance_name" {
 # # EBS
 # #   - 20G should be fine for dev.
 # # ---------------------------------------------------
-# resource "tfe_variable" "alpha_ebs_volume_size" {
+# resource "tfe_variable" "test_azure_ebs_volume_size" {
 #   key          = "candy_ebs_volume_size"
 #   value        = "20"
 #   category     = "terraform"
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_ebs_volume_type" {
+# resource "tfe_variable" "test_azure_ebs_volume_type" {
 #   key          = "candy_ebs_volume_type"
 #   value        = "gp2"
 #   category     = "terraform"
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_ebs_encrypted" {
+# resource "tfe_variable" "test_azure_ebs_encrypted" {
 #   key          = "candy_ebs_encrypted"
 #   value        = true
 #   category     = "terraform"
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_ebs_kms_key_id" {
+# resource "tfe_variable" "test_azure_ebs_kms_key_id" {
 #   key          = "candy_ebs_kms_key_id"
 #   value        = "18205e1b-9b4f-4df0-ab4e-a5b2c9de07a4"
 #   category     = "terraform"
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_ebs_delete_on_termination" {
+# resource "tfe_variable" "test_azure_ebs_delete_on_termination" {
 #   key          = "candy_ebs_delete_on_termination"
 #   value        = true
 #   category     = "terraform"
@@ -137,14 +137,14 @@ resource "tfe_variable" "alpha_instance_name" {
 # # ===================================================
 # # Subnet
 # # ---------------------------------------------------
-# resource "tfe_variable" "alpha_subnet_cidr_block_prefix" {
+# resource "tfe_variable" "test_azure_subnet_cidr_block_prefix" {
 #   key          = "candy_subnet_cidr_block_prefix"
 #   value        = "172.31."
 #   category     = "terraform"
 #   workspace_id = data.tfe_workspace.test-azure.id
 # }
 
-# resource "tfe_variable" "alpha_subnet_cidr_starting_address" {
+# resource "tfe_variable" "test_azure_subnet_cidr_starting_address" {
 #   key          = "candy_subnet_cidr_starting_address"
 #   value        = 210
 #   category     = "terraform"
@@ -155,7 +155,7 @@ resource "tfe_variable" "alpha_instance_name" {
 # # ===================================================
 # # SG
 # # ---------------------------------------------------
-# resource "tfe_variable" "alpha_sg_description" {
+# resource "tfe_variable" "test_azure_sg_description" {
 #   key          = "candy_sg_description"
 #   value        = "Validator Node Security Group"
 #   category     = "terraform"
