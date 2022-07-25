8936b8d67ebc0e66e829c431515f4bea19f7ed09
Wade Barnes <wade@neoterictech.ca>
2022-07-25 15:47:43 -0700

    Add prod settings
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>
    Signed-off-by: Sylvain Martel <sylvain.martel10@mcn.gouv.qc.ca>

--- terraform/meta/prod-aws-variables.tf
+++ terraform/meta/prod-aws-variables.tf
@@ -1,82 +1,82 @@
 # ===================================================
 # General
 # ---------------------------------------------------
-resource "tfe_variable" "test_aws_instance_count" {
+resource "tfe_variable" "prod_aws_instance_count" {
   key          = "candy_instance_count"
   value        = 2
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_use_elastic_ips" {
+resource "tfe_variable" "prod_aws_use_elastic_ips" {
   key          = "candy_use_elastic_ips"
   value        = true
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_region" {
+resource "tfe_variable" "prod_aws_region" {
   key          = "candy_region"
   value        = "ca-central-1"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_application_name" {
+resource "tfe_variable" "prod_aws_application_name" {
   key          = "candy_application_name"
   value        = "candy"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_environment" {
+resource "tfe_variable" "prod_aws_environment" {
   key          = "candy_environment"
-  value        = "test"
+  value        = "prod"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_instance_name" {
+resource "tfe_variable" "prod_aws_instance_name" {
   key          = "candy_instance_name"
-  value        = "test-bc-aws"
+  value        = "prod-bc-aws"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 # ===================================================
 
 # ===================================================
 # AMI
 # ---------------------------------------------------
-resource "tfe_variable" "test_aws_ami_owners" {
+resource "tfe_variable" "prod_aws_ami_owners" {
   key          = "candy_ami_owners"
   value        = "[\"099720109477\"]"
   hcl          = true
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_ami_filter_name" {
+resource "tfe_variable" "prod_aws_ami_filter_name" {
   key          = "candy_ami_filter_name"
   value        = "[\"ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*\"]"
   hcl          = true
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_ami_filter_root_device_type" {
+resource "tfe_variable" "prod_aws_ami_filter_root_device_type" {
   key          = "candy_ami_filter_root_device_type"
   value        = "[\"ebs\"]"
   hcl          = true
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_ami_filter_virtualization_type" {
+resource "tfe_variable" "prod_aws_ami_filter_virtualization_type" {
   key          = "candy_ami_filter_virtualization_type"
   value        = "[\"hvm\"]"
   hcl          = true
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 # ===================================================
 
@@ -84,11 +84,11 @@ resource "tfe_variable" "test_aws_ami_filter_virtualization_type" {
 # EC2
 #   - t3.large (for dev and test): 2CPU, 8GiB Memory
 # ---------------------------------------------------
-resource "tfe_variable" "test_aws_ec2_instance_type" {
+resource "tfe_variable" "prod_aws_ec2_instance_type" {
   key          = "candy_ec2_instance_type"
   value        = "t3.large"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 # ===================================================
 
@@ -96,85 +96,74 @@ resource "tfe_variable" "test_aws_ec2_instance_type" {
 # EBS
 #   - 20G should be fine for dev.
 # ---------------------------------------------------
-resource "tfe_variable" "test_aws_ebs_volume_size" {
+resource "tfe_variable" "prod_aws_ebs_volume_size" {
   key          = "candy_ebs_volume_size"
   value        = "20"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_ebs_volume_type" {
+resource "tfe_variable" "prod_aws_ebs_volume_type" {
   key          = "candy_ebs_volume_type"
   value        = "gp2"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_ebs_encrypted" {
+resource "tfe_variable" "prod_aws_ebs_encrypted" {
   key          = "candy_ebs_encrypted"
   value        = true
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_ebs_kms_key_id" {
+resource "tfe_variable" "prod_aws_ebs_kms_key_id" {
   key          = "candy_ebs_kms_key_id"
-  value        = ""
+  value        = "18205e1b-9b4f-4df0-ab4e-a5b2c9de07a4"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_ebs_delete_on_termination" {
+resource "tfe_variable" "prod_aws_ebs_delete_on_termination" {
   key          = "candy_ebs_delete_on_termination"
   value        = true
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 # ===================================================
 
 # ===================================================
 # Subnet
 # ---------------------------------------------------
-resource "tfe_variable" "test_aws_subnet_cidr_block_prefix" {
+resource "tfe_variable" "prod_aws_subnet_cidr_block_prefix" {
   key          = "candy_subnet_cidr_block_prefix"
   value        = "172.31."
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_subnet_cidr_starting_address" {
+resource "tfe_variable" "prod_aws_subnet_cidr_starting_address" {
   key          = "candy_subnet_cidr_starting_address"
   value        = 100
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
-}
-# ===================================================
-
-# ===================================================
-# SG
-# ---------------------------------------------------
-resource "tfe_variable" "test_aws_sg_description" {
-  key          = "candy_sg_description"
-  value        = "Validator Node Security Group"
-  category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 # ===================================================
 
 # ===================================================
 # Networking
 # ---------------------------------------------------
-resource "tfe_variable" "test_aws_client_port" {
+resource "tfe_variable" "prod_aws_client_port" {
   key          = "candy_client_port"
   value        = "9702"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 
-resource "tfe_variable" "test_aws_node_port" {
+resource "tfe_variable" "prod_aws_node_port" {
   key          = "candy_node_port"
   value        = "9701"
   category     = "terraform"
-  workspace_id = data.tfe_workspace.test-aws.id
+  workspace_id = data.tfe_workspace.prod-aws.id
 }
 # ===================================================
