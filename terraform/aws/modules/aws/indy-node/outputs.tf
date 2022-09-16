56b876e1630835b5a97b555838b891da00440865
Wade Barnes <wade@neoterictech.ca>
2022-09-16 14:49:16 -0700

    Update configurations
    - Refine node seed generation.
    - Mount data volume to /var/lib/indy
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- terraform/aws/modules/aws/indy-node/outputs.tf
+++ terraform/aws/modules/aws/indy-node/outputs.tf
@@ -1,3 +1,11 @@
 output "node_info" {
-  value = [aws_instance.indy_node.public_ip, aws_ebs_volume.data_volume.id, random_id.node_seed.b64_url]
+  value = [
+    # The node's public IP address.  Used by Ansible for configuring the node.
+    aws_instance.indy_node.public_ip,
+    # data_volume_id
+    aws_ebs_volume.data_volume.id,
+    # node_seed
+    # Remove special characters and crop to into a 32 character seed.
+    substr(replace(replace(random_id.node_seed.b64_url, "_", ""), "-", ""), 0, 32)
+    ]
 }
